95c1e383915de0efd1e1e0f5452e5b5dbfc9f019
arcodez <abelileitor123@gmail.com>
2021-03-07 23:12:29 +0100

    add mongodb config

---
+++ next.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    env: {
+        mongodburl: "Your Mongo Bd connection"
+    }
+};
---
+++ pages/api/user.js
@@ -0,0 +1,3 @@
+import connectDB from '../../middlewar/mongodb'
+import connectDB from '../../middlewar/mongodb'
+import connectDB from '../../middlewar/mongodb'
