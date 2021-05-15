77d922566ea038542e0d9161a1aa7cf01ee42ce7
Abel Rodriguez <abelileitor123@gmail.com>
2021-05-15 16:14:46 -0430

    update

--- pages/api/musica/[id].js
+++ pages/api/musica/[id].js
@@ -6,14 +6,5 @@ export default async (req, res) => {
   const { query } = req;
   const { id } = query;
   const music = await Music.findById(id);
-    res.json(id)
-  /* Aqui Viene el codigo de la bd */
-/*   try {
-  
-    if (!music) response.status(404).send("No item found");
-    response.status(200).send();
-  } catch (error) {
-    response.status(500).send(error);
-  }
+  res.json(music);
 };
- */
