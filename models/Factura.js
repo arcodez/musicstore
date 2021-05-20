5bc2b6e48c512d995ba18bfaab5832bfb6ef23fa
Abel Rodriguez <abelileitor123@gmail.com>
2021-05-19 20:38:25 -0430

    udpdate Models and fronted and Test props

---
+++ models/Factura.js
@@ -0,0 +1,18 @@
+import mongoose, { models } from "mongoose";
+
+const FacturaSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  hora: {
+    type: Date.now,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports =  mongoose.models.Note || mongoose.model('Note', FacturaSchema);
--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,6 @@ import Contact from "../components/Contact";
 import Horarios from "../components/Horarios";
 import Layout from "../components/Layout/Layout";
 import Modal from "../components/Modal";
-import Head from "next/head";
 import Hero from "../components/Hero";
 
 export default function index() {
