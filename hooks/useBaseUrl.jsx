4dfff9f012c21d896bf3cb9f11ccfd99a167c400
arcodez <abelileitor123@gmail.com>
2021-05-28 13:42:38 -0400

    Update files

---
+++ hooks/useBaseUrl.jsx
@@ -0,0 +1,8 @@
+import { useState, useEffect } from "react";
+
+function useBaseUrl() {
+  const
+  return <h1>{baseUrl}</h1>;
+}
+
+export default useBaseUrl;
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import About from "../components/About";
 import Class from "../components/Class";
 import Contact from "../components/Contact";
@@ -8,11 +8,16 @@ import Modal from "../components/Modal";
 import Hero from "../components/Hero";
 
 export default function index() {
+  const [baseurl, setBaseUrl] = useState("");
+  useEffect(() => {
+    setBaseUrl(window.location.hostname);
+  }, []);
   return (
     <Layout>
       <Hero />
       <About />
       <Class />
+      {baseurl}
       <Horarios />
       <Contact />
       <Modal />
