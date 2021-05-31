75a3f8a4dbe34b7b6724817ab62bd5b8e5a24c1d
arcodez <abelileitor123@gmail.com>
2021-05-31 14:44:20 -0400

    Deleted files

--- hooks/useAxiosSSR.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function useAxiosSSR() {
-    return (
-        
-    )
-}
-
-export default useAxiosSSR
--- hooks/useBaseUrl.jsx
+++
@@ -1,8 +0,0 @@
-import { useState, useEffect } from "react";
-
-function useBaseUrl() {
-  const
-  return <h1>{baseUrl}</h1>;
-}
-
-export default useBaseUrl;
