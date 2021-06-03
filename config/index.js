4363a59e222c07d3bdf9ffd770417f1c9103243c
arcodez <abelileitor123@gmail.com>
2021-06-02 20:23:40 -0400

    Update temp endpoint

--- config/index.js
+++ config/index.js
@@ -2,4 +2,4 @@ const dev = process.env.NODE_ENV !== "production";
 
 export const server = dev
   ? "http://localhost:3000"
-  : "https://musicstore-arcodez.vercel.app/";
+  : "https://oelif.sse.codesandbox.io/";
